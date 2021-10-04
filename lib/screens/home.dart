5766cf74522544c06724904a4976e508d0b17f25
tosif91 <61594032+tosif91@users.noreply.github.com>
2021-10-04 17:18:33 +0530

    add two new screens

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:learningflutter/screens/todo_add.dart';
+import 'package:learningflutter/screens/todo_item.dart';
 import 'package:learningflutter/widgets/card_item.dart';
+
+List<String> messageData = <String>[];
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -12,46 +16,33 @@ class _HomeState extends State<Home> {
   List<String> messageData = <String>[];
 
   TextEditingController textEditingController = TextEditingController();
-
+  int bottomIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("To DO"),
-          centerTitle: true, //to make title in center
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment
-              .end, // aligne item in end ( right side of screen for columns)
-          children: [
-            TextField(
-              controller: textEditingController,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (textEditingController.text.isNotEmpty) {
-                      messageData.add(
-                          textEditingController.text); //added value in list
+      appBar: AppBar(
+        title: const Text("To DO"),
+        centerTitle: true, //to make title in center
+      ),
 
-                      textEditingController.clear(); // clear value of textfield
-                    }
-                  });
-                },
-                child: const Text('Save'),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: messageData.length,
-                itemBuilder: (BuildContext context, int index) => CardItem(
-                  message: messageData[messageData.length - 1 - index],
-                ),
-              ),
-            ),
-          ],
-        ));
+      //todo_add & todo_item
+
+      body: (bottomIndex == 0) ? TodoItem() : TodoAdd(),
+
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: bottomIndex,
+          onTap: (index) {
+            print('this is $index');
+            setState(() {
+              bottomIndex = index;
+            });
+          },
+          items: <BottomNavigationBarItem>[
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.list), label: 'my todo'),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.add), label: 'add todo'),
+          ]),
+    );
   }
 }
---
+++ lib/screens/todo_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TodoItem extends StatelessWidget {
+  const TodoItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("this is items"),
+    );
+  }
+}
