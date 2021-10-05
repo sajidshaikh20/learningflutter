81fdd95f4d986f38279734dea236571a58c6106b
sajid shaikh <89159945+sajidshaikh20@users.noreply.github.com>
2021-10-05 15:37:45 +0530

    bhuru changes

--- lib/screens/todo_item.dart
+++ lib/screens/todo_item.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:learningflutter/screens/home.dart';
+import 'package:learningflutter/widgets/card_item.dart';
 
 class TodoItem extends StatelessWidget {
   const TodoItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("this is items"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+            itemCount: messageData.length,
+            itemBuilder: (context, index) {
+              return CardItem(
+                message: messageData[index],
+              );
+            },
+          ))
+        ],
+      ),
     );
   }
 }
