0de17274bfd287fe1e3c233af5f307282c459f76
tosif91 <61594032+tosif91@users.noreply.github.com>
2021-10-05 17:31:47 +0530

    bottom index fix

--- lib/widgets/card_item.dart
+++ lib/widgets/card_item.dart
@@ -8,16 +8,31 @@ class CardItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.white54,
       elevation: 2.0,
-      shadowColor: Colors.purple,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      // shadowColor: Colors.purple,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: Padding(
         padding: const EdgeInsets.all(8),
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text(message),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+              child: Text('5 Oct 2021'),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  message,
+                  style: TextStyle(fontSize: 17.0),
+                ),
+              ),
+            ),
             Row(
               // crossAxisAlignment: CrossAxisAlignment.end,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -29,7 +44,7 @@ class CardItem extends StatelessWidget {
                 ),
                 ElevatedButton(
                   onPressed: () {},
-                  child: Text("delete"),
+                  child: Text("Delete"),
                   style: ElevatedButton.styleFrom(primary: Colors.red),
                 ),
               ],
