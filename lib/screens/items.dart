062fd5b82de2aab8ba1fc090ef4f08aedcea9980
sajid shaikh <89159945+sajidshaikh20@users.noreply.github.com>
2021-10-08 15:29:00 +0530

    update

--- lib/screens/items.dart
+++ lib/screens/items.dart
@@ -2,25 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:learningflutter/model/todo_model.dart';
 import 'package:learningflutter/widgets/todo_card.dart';
 
-class Items extends StatelessWidget {
+class Items extends StatefulWidget {
   Items({Key? key}) : super(key: key);
 
+  @override
+  _ItemsState createState() => _ItemsState();
+}
+
+class _ItemsState extends State<Items> {
   List<TodoModel> data = <TodoModel>[
     TodoModel(
         date: '21 oct 2021',
-        title: 'this is my title',
-        message: 'this is my message'),
+        title: 'Tution',
+        message: 'Resume submit kerna hai or tution jana hai.'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      itemCount: data.length,
       itemBuilder: (context, index) {
         return TodoCard(
           modelObj: data[index],
         );
       },
-      itemCount: data.length,
     );
   }
 }
--- lib/widgets/todo_card.dart
+++ lib/widgets/todo_card.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:learningflutter/model/todo_model.dart';
 
-class TodoCard extends StatelessWidget {
+class TodoCard extends StatefulWidget {
   TodoCard({Key? key, required this.modelObj}) : super(key: key);
 
   TodoModel modelObj;
 
+  @override
+  _TodoCardState createState() => _TodoCardState();
+}
+
+class _TodoCardState extends State<TodoCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,26 +20,38 @@ class TodoCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                modelObj.date,
-                style: TextStyle(color: Colors.black38),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.modelObj.title,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                  maxLines: 2,
+                ),
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    widget.modelObj.date,
+                    style: TextStyle(color: Colors.black38),
+                  ),
+                ),
+              ],
             ),
             const SizedBox(
               height: 10,
             ),
             Text(
-              modelObj.title,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-              maxLines: 2,
+              widget.modelObj.message * 3,
+              style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(modelObj.message)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+              ],
+            )
           ],
         ),
       ),
